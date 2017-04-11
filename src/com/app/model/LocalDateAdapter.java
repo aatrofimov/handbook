ee791d1592c220395929bf4c42fb399b8ca59627
Alexey Trofimov <TrofimovAA@sib-soft.ru>
2017-04-11 13:04:19 +0700

    добавил xmlAdapter для LocalDate

---
+++ src/com/app/model/LocalDateAdapter.java
@@ -0,0 +1,26 @@
+package com.app.model;
+
+import javax.xml.bind.annotation.adapters.*;
+import java.time.*;
+
+/**
+ * Created by IntelliJ IDEA.<br>
+ * User: TrofimovAA<br>
+ * Date: 11.04.2017<br>
+ * Time: 6:49<br>
+ * xml адаптер для типа LocalDate
+ */
+public class LocalDateAdapter
+	extends XmlAdapter<String, LocalDate> {
+
+	@Override
+	public LocalDate unmarshal(String v) throws Exception {
+		return LocalDate.parse(v);
+	}
+
+	@Override
+	public String marshal(LocalDate v) throws Exception {
+		return v.toString();
+	}
+
+}
---
+++ src/com/app/model/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.<br>
+ * User: TrofimovAA<br>
+ * Date: 11.04.2017<br>
+ * Time: 6:52<br>
+ * Регистрация xml адаптера
+ */
+@XmlJavaTypeAdapter(type = LocalDate.class,
+	value = LocalDateAdapter.class)
+package com.app.model;
+
+import javax.xml.bind.annotation.adapters.*;
+import java.time.*;
